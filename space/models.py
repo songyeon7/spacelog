2cac6ea05d0ee80b21edc0c5053bbe10c5399c44
songyeon7 <song-0708@naver.com>
2023-11-08 10:30:00 +0900

    1108

--- space/models.py
+++ space/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class space(models.Model):
+    title=models.CharField(max_length=50)
+    author=models.CharField(max_length=50)
+    image=models.CharField(max_length=400)
+    contents=models.TextField()
+    
+    
