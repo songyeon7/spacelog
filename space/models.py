74e0885cf7a92fa258b2d4aa54df076c7189a036
songyeon7 <97648066+songyeon7@users.noreply.github.com>
2023-11-13 00:16:00 +0900

    CREATE FILE

--- space/models.py
+++ space/models.py
@@ -1,10 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class space(models.Model):
-    title=models.CharField(max_length=50)
-    author=models.CharField(max_length=50)
-    image=models.CharField(max_length=400)
-    contents=models.TextField()
-    
-    
