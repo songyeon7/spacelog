b8b3066b80fd3a35bca377f871d3f1318966976f
songyeon7 <97648066+songyeon7@users.noreply.github.com>
2023-11-20 03:24:17 +0900

    migrate

--- space/models.py
+++ space/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
-# Create your models here.
 class Space(models.Model):
-    title=models.CharField(max_length=200)
-    author=models.CharField(max_length=100)
-    context=models.TextField()
-    create_date=models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    context = models.TextField()
+    create_date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='images/')  # 새로운 이미지 필드 추가
 
     def __str__(self):
         return self.title
