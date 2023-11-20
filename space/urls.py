321ad0713b9438f6c16dd343ee81b15b880712d5
songyeon7 <97648066+songyeon7@users.noreply.github.com>
2023-11-20 09:38:11 +0900

    경로수정포기

--- space/urls.py
+++ space/urls.py
@@ -1,16 +1,6 @@
-from django.contrib import admin
-from django.urls import path, include
-from .views import *
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('<str:id>', detail, name="detail"),
-    path('new/', new, name="new"),
-    path('create/', create, name="create"),
-    path('edit/<str:id>', edit, name="edit"),
-    path('update/<str:id>', update, name="update"),
-    path('delete/<str:id>', delete, name="delete"),
+    path('', views.home, name = 'home'),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- spacelog/urls.py
+++ spacelog/urls.py
@@ -17,9 +17,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from space.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name="home"),
-    path('space/', include('space.urls'))
+    path('<str:id>', detail, name="detail"),
+    path('new/', new, name="new"),
+    path('create/', create, name="create"),
+    path('edit/<str:id>', edit, name="edit"),
+    path('update/<str:id>', update, name="update"),
+    path('delete/<str:id>', delete, name="delete"),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
