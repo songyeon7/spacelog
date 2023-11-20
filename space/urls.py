1fc564add072e59a8afd352a5bf1d98809af13df
songyeon7 <97648066+songyeon7@users.noreply.github.com>
2023-11-20 09:24:01 +0900

    경로수정

--- space/urls.py
+++ space/urls.py
@@ -1,6 +1,16 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
+from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.space_home, name = 'space_home'),
+    path('<str:id>', detail, name="detail"),
+    path('new/', new, name="new"),
+    path('create/', create, name="create"),
+    path('edit/<str:id>', edit, name="edit"),
+    path('update/<str:id>', update, name="update"),
+    path('delete/<str:id>', delete, name="delete"),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
