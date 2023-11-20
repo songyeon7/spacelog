71c06a0030a32674a0d0c11b9a0a5d64442cc0e6
songyeon7 <97648066+songyeon7@users.noreply.github.com>
2023-11-20 09:14:48 +0900

    템플릿 적용

--- spacelog/settings.py
+++ spacelog/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'spacelog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['spacelog/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
